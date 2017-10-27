4bd6872d963aaecda6a6aa0c621ddc1f23cb4368
Freddie Ofori <FrederickOfori@email.com>
2017-10-27 14:37:38 +0100

    scores added

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -2,9 +2,14 @@ describe("Game", function() {
     beforeEach(function(){
       game = new Game()
     });
+       
+        it("should have 2 players, whose scores start at 0", function(){
+            expect(game.scores).toEqual([0,0]);
+        });
 
         it("should have a dice that rolls numbers between 1 & 6", function(){
-            expect(game.dice >= 1 && game.dice <= 6).toBeTruthy()
+            expect(game.dice >= 1 && game.dice <= 6).toBeTruthy();
         });
+
 
 });
--- src/app.js
+++ src/app.js
@@ -2,4 +2,6 @@ function Game(){
 
 }
 
-Game.prototype.dice = 5
+Game.prototype.dice = Math.floor(Math.random() * 6) + 1;
+Game.prototype.scores = [0,0];
+
